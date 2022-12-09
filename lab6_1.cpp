623152bd698043abbcbb1d6a3e61facdf4fa2d66
= <=>
2022-12-09 15:16:03 +0700

    แก้ไขโจทย์

---
+++ lab6_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num=1,even=-1,odd=0;
+
+    while(num != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num%2 == 0)
+        {
+            even++;
+        }else{
+            odd++;
+        }
+      
+    }
+        cout << "#Even numbers = " << even << "\n";
+        cout << "#Odd numbers = " << odd;
+
+
+    
+    return 0;
+}
